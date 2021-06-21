dbec18ceeca653ab862b9bdbea76fbe0177f1152
ColegioInnova <84755188+ColegioInnova@users.noreply.github.com>
2021-06-21 17:36:32 -0500

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('.menu-btn').addEventListener('click', () => {
-    document.querySelector('.nav-menu').classList.toggle('show');
-});
-
-//llamamos a las clases htm para darle efecto con ScrollReveal
-ScrollReveal().reveal('.showcase');
-ScrollReveal().reveal('.news-cars', { delay: 500 });
-ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
-ScrollReveal().reveal('.cards-banner-two', { delay: 500 });
